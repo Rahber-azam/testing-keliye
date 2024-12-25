8f424fd90e6034304443934d07aa1ea84f894f82
Rahber-azam <146512758+Rahber-azam@users.noreply.github.com>
2024-12-25 23:27:21 +0530

    Update A.java

--- IIB/A.java
+++ IIB/A.java
@@ -3,5 +3,6 @@ package newkeyword.IIB;
 public class A {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world2");
     }
 }
