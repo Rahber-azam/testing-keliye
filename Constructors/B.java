8c9a3d110192a23a61bac70629282a3ed822c224
Rahber Azam <rahbera414@gmail.com>
2025-03-16 00:13:04 +0530

    done

---
+++ Constructors/B.java
@@ -0,0 +1,12 @@
+package newkeyword.Constructors;
+//parameterize
+public class B {
+    B(int a,String b){
+        System.out.println(a +" "+ b);
+
+    }
+
+    public static void main(String[] args) {
+        B b=new B(10,"hello");
+    }
+}
